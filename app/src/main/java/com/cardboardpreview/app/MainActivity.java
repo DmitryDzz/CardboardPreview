3f34ae4a224e7025651ee7971d8775920f323e19
DmitryDzz <dzakhovds@gmail.com>
2016-01-18 11:56:45 +0300

    Cardboard SDK was added

--- app/src/main/java/com/cardboardpreview/app/MainActivity.java
+++ app/src/main/java/com/cardboardpreview/app/MainActivity.java
@@ -1,13 +1,22 @@
 package com.cardboardpreview.app;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends CardboardActivity {
+
+    private final VrStereoRenderer mStereoRenderer = new VrStereoRenderer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        // Associate a CardboardView.StereoRenderer with cardboardView.
+        cardboardView.setRenderer(mStereoRenderer);
+        // Associate the cardboardView with this activity.
+        setCardboardView(cardboardView);
     }
 }
---
+++ app/src/main/java/com/cardboardpreview/app/VrStereoRenderer.java
@@ -0,0 +1,43 @@
+package com.cardboardpreview.app;
+
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+/**
+ * Created by dmitrydzz on 18.01.16.
+ */
+public class VrStereoRenderer implements CardboardView.StereoRenderer {
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+}
